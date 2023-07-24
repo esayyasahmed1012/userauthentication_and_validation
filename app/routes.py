6d516a7dd84bd818b1475df0219b4ae54dbf3dd0
esayyas <esayyasahmed1012@gmail.com>
2023-07-25 00:44:51 +0300

    routes.py

--- app/routes.py
+++ app/routes.py
@@ -23,6 +23,18 @@ from app.form.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostF
 #     }
 # ]
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+@app.errorhandler(403)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('403.html'), 403
+@app.errorhandler(500)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('500.html'), 500
 
 @app.route("/")
 @app.route("/home")
